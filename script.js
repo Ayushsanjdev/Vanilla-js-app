e650fc9620b076587565e12d84b54ee176f6350a
Ayush Sanj <ayushsanjpro@gmail.com>
2021-04-12 16:47:47 +0530

    added style

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var textInput = document.querySelector(".text-input").nodeValue;
-console.log(textInput);
+// var textInput = document.querySelector("#translate").nodeValue;
+// console.log(textInput);
 
-var btn = document.querySelector("#btn").addEventListener("click", function mouseClick() {
-    console.log("clicked!!", textInput);
-});
+var btn = document.querySelector("#btn").addEventListener("click", mouseClick());
+
+function mouseClick() {console.log("clicked!!");}
