982311cf0d6a205d86160c8fff1512f8fd495822
Ayush Sanj <ayushsanjpro@gmail.com>
2021-04-12 15:33:52 +0530

    import js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var textInput = document.querySelector(".text-input").nodeValue;
+console.log(textInput);
+
 var btn = document.querySelector("#btn").addEventListener("click", function mouseClick() {
-    console.log("clicked!!");
+    console.log("clicked!!", textInput);
 });
