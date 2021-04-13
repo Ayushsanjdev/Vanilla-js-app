06e09f3fc77c954f08a5c095874441c28c613059
Ayush Sanj <ayushsanjpro@gmail.com>
2021-04-13 14:45:52 +0530

    fetched: server

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 // var textInput = document.querySelector("#translate").nodeValue;
 // console.log(textInput);
 
-var btn = document.querySelector("#btn");
-var textInput = document.querySelector(".text-input");
-var textOutput = document.querySelector("#output-div");
+let btn = document.querySelector("#btn");
+let textInput = document.querySelector(".text-input");
+let textOutput = document.querySelector("#output-div");
 
+// var serverURL = "https://lesonfourapi.tanaypratap.repl.co/translate/yoda.json?text=I m Ayush Sanj";
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslateURL(text) {
+    return serverURL + "?" + "text" + text;
+}
 
 function clickHandle() {
-    textOutput.innerText = "lang " + textInput.value;
-    parent.appendChild(textOutput);
-}
+    let inputText = textInput.value;
+    
+    fetch(getTranslateURL(inputText))
+    .then(response => response.json())
+    .then(json => {let translatedText = json.contents.translated;
+    textOutput.innerText = translatedText})
+    .catch((error) => console.log("error: " + error)); 
+    alert("some error occured try again later somtime");
+
+    
+};
 
 btn.addEventListener("click", clickHandle);
 
