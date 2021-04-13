d08ad77619cdcb87333d7ed75a0f6a970175e82c
Ayush Sanj <ayushsanjpro@gmail.com>
2021-04-13 16:57:13 +0530

    Done with script : but still Fake api

--- script.js
+++ script.js
@@ -5,24 +5,21 @@ let btn = document.querySelector("#btn");
 let textInput = document.querySelector(".text-input");
 let textOutput = document.querySelector("#output-div");
 
-// var serverURL = "https://lesonfourapi.tanaypratap.repl.co/translate/yoda.json?text=I m Ayush Sanj";
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=I m Ayush Sanj";
 
-var serverURL = "https://api.funtranslations.com/translate/minion.json";
+// var serverURL = "https://api.funtranslations.com/translate/minion.json?text=I m Ayush Sanj";
 
-function getTranslateURL(text) {
-    return serverURL + "?" + "text" + text;
+function getTranslateURL(input) {
+    return serverURL + "?" + "text" + input;
 }
 
 function clickHandle() {
     let inputText = textInput.value;
     //fetching server
     fetch(getTranslateURL(inputText))
-    .then(response => response.json())
-    .then(json => {let translatedText = json.contents.translated;
-    textOutput.innerText = translatedText})
-    .catch((error) => console.log("error: " + error)); 
-    alert("some error occured try again later somtime");
+        .then(response => response.json())
+        .then(json => textOutput.innerText = json.contents.translated)
+        .catch((error) => console.log("error: " + error));
 };
 
 btn.addEventListener("click", clickHandle);
-
