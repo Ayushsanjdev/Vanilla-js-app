7243ccb3b7c5b934e258cc756f320f65398f1434
Ayush Sanj <ayushsanjpro@gmail.com>
2021-04-13 14:46:32 +0530

    added: comments

--- script.js
+++ script.js
@@ -15,15 +15,13 @@ function getTranslateURL(text) {
 
 function clickHandle() {
     let inputText = textInput.value;
-    
+    //fetching server
     fetch(getTranslateURL(inputText))
     .then(response => response.json())
     .then(json => {let translatedText = json.contents.translated;
     textOutput.innerText = translatedText})
     .catch((error) => console.log("error: " + error)); 
     alert("some error occured try again later somtime");
-
-    
 };
 
 btn.addEventListener("click", clickHandle);
